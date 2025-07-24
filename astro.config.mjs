75cc7f5fb5872f48f3b78815d382175bc83e1f3f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-24 15:10:31 -0400

    Removed specific packages for compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { compress } from '@playform/compress';
+import compress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
