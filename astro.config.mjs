52f2984782cf9973a1571ddd28963ffaaf4e047c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-24 15:08:31 -0400

    reaplaced astrocompress with playform/compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
+import { compress } from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
