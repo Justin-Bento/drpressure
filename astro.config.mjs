0e83926af28654774acb46b5e733802c1a2148a1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-29 14:16:44 -0400

    Added keystatic to astro integrations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import alpinejs from '@astrojs/alpinejs';
 
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
-  integrations: [alpinejs(), react(), markdoc()],
+  integrations: [alpinejs(), react(), markdoc(), keystatic()],
 });
