7f0e99d4c2d940d66960a8bd417c113fcaa2b41b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-08-04 18:07:44 -0400

    deleted files in the wrong place.

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -17,7 +17,7 @@ export default config({
     }),
     testimonials: collection({
       label: 'Testimonials',
-      path: 'src/content/posts/testimonials/*',
+      path: 'src/content/testimonials/*',
       slugField: 'name',
       schema: {
         name: fields.text({ label: 'Author' }),
