20eb27d30d191d5be9d56cbde09a8d427178d4ab
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-08-04 21:38:57 -0400

    added about page singleton file

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -35,5 +35,38 @@ export default config({
         headline: fields.text({ label: 'HomeText' }),
       },
     }),
+    Aboutpage: singleton({
+      label: 'About',
+      path: 'src/content/singleton/aboutpage',
+      schema: {
+        headline: fields.text({ label: 'About Summary' }),
+        mission: fields.text({ label: 'Our Mission', multiline: true }),
+        years: fields.date({
+          label: 'Event date',
+          description: 'The date of the event',
+        }),
+        experts: fields.number({ label: 'Number Of Experts' }),
+        projects: fields.number({ label: 'Projects Completed' }),
+        awards: fields.number({ label: 'Awards Recived' }),
+        values: fields.array(
+          fields.object({
+            name: fields.text({ label: 'Value' }),
+            message: fields.text({ label: 'Name', multiline: true }),
+          }),
+          {
+            label: 'Company Values',
+          }
+        ),
+        team: fields.array(
+          fields.object({
+            name: fields.text({ label: 'Employee Name' }),
+            position: fields.text({ label: 'position', multiline: true }),
+          }),
+          {
+            label: 'Meet Our Team',
+          }
+        ),
+      },
+    }),
   },
 });
